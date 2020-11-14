4243131c47fff09259ed9a3079385cd8bdeeac9d
Yohan Jo <yohanj@nairobi.sp.cs.cmu.edu>
2020-11-14 01:51:08 -0500

    Updated run_clf_bertpy

--- src/run_clf_bert.py
+++ src/run_clf_bert.py
@@ -1,15 +1,16 @@
 import os
 import sys
 
-mode = sys.argv[1]
-n_trials = 10
-n_epochs = 4
-feats_path = "../data/feat-combined.csv"
-data_path = "../data/posts-sents.csv"
-domain_adapt = ""
-domain = "domain40"
-fusion_dim = 0
-feats = ""
+mode = sys.argv[1]  # "attack" for predicting attacked vs. unattacked
+                    # "success" for predicting successfully attacked vs. not
+n_trials = 10  # Num of total runs
+n_epochs = 4  # Num of epochs for each run
+feats_path = "../data/feat-combined.csv"  # Feature file
+data_path = "../data/posts-sents.csv"  # Sentence file
+domain_adapt = ""  # Do not modify
+domain = "domain40"  # Do not modify
+fusion_dim = 0  # Do not modify
+feats = ""  # Features to include (do not modify)
 cmd = f"~/anaconda/bin/python clf_bert.py -mode {mode} -name domain -feat_names {feats} -domain {domain} {domain_adapt} -fusion_dim {fusion_dim} -weight_classes -n_trials {n_trials} -n_epochs {n_epochs} -feats {feats_path} -data {data_path}"
 print(cmd)
 os.system(cmd)
